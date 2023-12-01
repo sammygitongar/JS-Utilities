15b2d1be274ffa2a514c381905e95a5e69bd4406
Samuel Gitonga <sammygitongar@gmail.com>
2023-12-01 10:23:05 +0300

    / ============Changing the contents of an HTML element =================

--- prevent.js
+++ prevent.js
@@ -1,10 +1,18 @@
+// ===========Preventing Default Javascript Submit Behaviour of a Button========
+
 document.querySelector("#logButton").addEventListener('submit', function(event){
     event.preventDefault();
-
-   
-
   });
 
   function logFunction(){
       console.log("log clicked");
     }
+
+
+
+// ============Changing the contents of an HTML element =================
+
+function changeContents(){
+    const contents = document.querySelector(".initial-content");
+    contents.innerHTML = "The content has bee changed";
+}
