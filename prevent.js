ba0ef4a850dc7a3019023e59e5d875652409e6fa
Samuel Gitonga <sammygitongar@gmail.com>
2023-12-04 18:36:13 +0300

    Added JS Factorial Calculator

--- prevent.js
+++ prevent.js
@@ -1,34 +1,11 @@
-// ===========Preventing Default Javascript Submit Behaviour of a Button========
-
-document.querySelector("#logButton").addEventListener('submit', function(event){
-    event.preventDefault();
-  });
-
-  function logFunction(){
-      console.log("log clicked");
-    }
-
-
-
-// ============Changing the contents of an HTML element =================
-
-function changeContents(){
-    const contents = document.querySelector(".initial-content");
-    contents.innerHTML = "The content has bee changed";
-}
-
-// ================Changing the attribute of an HTML element====================
-function changeImage(){
-    const imageChanger = document.querySelector("#thisImage");
-    let imageUrl = imageChanger.src;
-    const lastIndexOfSlash = imageUrl.lastIndexOf("/");
-    const truncatedURL = imageUrl.substring(lastIndexOfSlash + 1);
-
-    console.log("Current URL:", imageUrl);
-    console.log("Truncated URL:", truncatedURL);
-    if(truncatedURL == "journal.jpg"){
-        imageChanger.src = "images/js.png";
-    } else {
-        imageChanger.src = "images/journal.jpg";
-    }
-}
+const http = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
