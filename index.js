f0f051bf46c3bda75127185015464bffff694d27
Samuel Gitonga <sammygitongar@gmail.com>
2023-12-06 07:44:41 +0300

    Finished the JS of my dumb shape creator

--- index.js
+++ index.js
@@ -11,3 +11,22 @@ const countName = function(){
     characters.innerHTML = `Your name has ${userNameCount} characters`;
 
 }
+
+
+// Example 1: Asynchronous Function with Callback
+function fetchData(callback) {
+    // Simulate an asynchronous operation (e.g., fetching data from a server)
+    setTimeout(function() {
+        const data = "This is the fetched data";
+        callback(data); // Call the callback function with the result
+    }, 2000); // Simulating a delay of 2000 milliseconds (2 seconds)
+}
+
+// Callback function
+function processData(data) {
+    console.log("Processing data:", data);
+}
+
+// Call the asynchronous function with the callback
+fetchData(processData);
+console.log("This line is executed before the asynchronous operation completes.");
--- prevent.js
+++ prevent.js
@@ -1,11 +1,57 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const http = require('node:http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// ================Dumb Game JS=====================
+
+let clickCounter = 0;
+          
+
+function createBlock(){
+
+    const dumb = document.querySelector(".dumb");
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    div.appendChild(p);
+
+
+
+
+        if(clickCounter < 4){
+            clickCounter++;
+        
+            dumb.appendChild(div);
+        
+        } else {
+            alert("Enough for now mate!");
+            window.location.reload();
+        };
+
+        if (clickCounter == 1){
+            div.style.background  = "red";
+            p.innerHTML = "Uno";
+
+        } else if (clickCounter == 2){
+            div.style.background  = "blue";
+            p.innerHTML = "Dos";
+
+        } else if (clickCounter == 3){
+            div.style.background  = "green";
+            p.innerHTML = "Tres";
+
+
+        } else {
+            div.style.background  = "black";
+            p.innerHTML = "Quatro";
+
+        }
+}
