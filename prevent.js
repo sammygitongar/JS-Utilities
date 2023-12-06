d3771b8ebe6b0d711bee724c26b73b8f0d33cfc8
Samuel Gitonga <sammygitongar@gmail.com>
2023-12-06 07:56:31 +0300

    Some updates on prevent.js

--- prevent.js
+++ prevent.js
@@ -10,6 +10,28 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
+// ============Change ELements Contents JS=============
+function changeContents(){
+    const contents = document.querySelector(".initial-content");
+    contents.innerHTML = "The content has bee changed";
+}
+
+
+// ================Changing the attribute of an HTML element====================
+function changeImage(){
+    const imageChanger = document.querySelector("#thisImage");
+    let imageUrl = imageChanger.src;
+    const lastIndexOfSlash = imageUrl.lastIndexOf("/");
+    const truncatedURL = imageUrl.substring(lastIndexOfSlash + 1);
+
+    console.log("Current URL:", imageUrl);
+    console.log("Truncated URL:", truncatedURL);
+    if(truncatedURL == "journal.jpg"){
+        imageChanger.src = "images/js.png";
+    } else {
+        imageChanger.src = "images/journal.jpg";
+    }
+ }
 
 // ================Dumb Game JS=====================
 
